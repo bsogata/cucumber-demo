6fb7cef74792b3afafe9d06e6bd3a3aac0f8a692
Branden Ogata <branden.s.ogata@gmail.com>
2015-12-05 16:51:29 -1000

    Initial commit without Cucumber testing.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+  def index
+    
+  end
+  
+  def cats_captions
+    
+  end
+  
+  def cats_without_captions
+    
+  end
+
+  def essays
+    
+  end
+
+  def quotes
+    
+  end
+  
+end
